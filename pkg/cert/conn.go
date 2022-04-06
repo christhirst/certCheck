aa7bb35f45a579a9c4eda8dd69000654c68a10e2
christhirst <chrisgluon@gmail.com>
2022-04-06 12:20:24 +0000

    added

---
+++ pkg/cert/conn.go
@@ -0,0 +1,28 @@
+package cert
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+)
+
+func con() {
+	conf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	conn, err := tls.Dial("tcp", "www.google.com:443", conf)
+	if err != nil {
+		log.Println("Error in Dial", err)
+		return
+	}
+	defer conn.Close()
+	certs := conn.ConnectionState().PeerCertificates
+	for _, cert := range certs {
+		fmt.Printf("Issuer Name: %s\n", cert.Issuer)
+		fmt.Printf("Expiry: %s \n", cert.NotAfter.Format("2006-January-02"))
+		fmt.Printf("Common Name: %s \n", cert.Issuer.CommonName)
+
+	}
+
+}
